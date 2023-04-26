f0f5fd8828e0d9d15244ecb6e91f592e90eda11d
Sergio Paniagua <abril09@outlook.com>
2023-04-25 19:28:03 -0600

    TS y TI terminados

--- tramas.c
+++ tramas.c
@@ -1,56 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define T_TRAMA 32
+#define T_TRAMA 64
 
 void main(){
                             /*        MAC DESTINO                      |   MAC ORIGEN                      |    ToT    |   DATOS
-                                                                       |                                   |           | CONTROL                   SS       */
-    unsigned char trama[T_TRAMA] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x00, 0x0E, 0x00, 0x0F,             // 00 RR
-                                    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};            // 01 RNR
+                                                                       |                                   |           | CONTROL               */
+    unsigned char trama[T_TRAMA] = {0x00, 0x02, 0xb3, 0x9c, 0xae, 0xba, 0x00, 0x02, 0xb3, 0x9c, 0xdf, 0x1b, 0x00, 0x03, 0xf0, 0xf0,
+				                    0x7f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,		// SS
+  				                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x43, 0x05, 0x90, 0x6d};            // 01 RNR
                                                                                                                                                 // 10 REJ
-                                                                                                                                                // 11 SREJ
-    short int tot = 0x0000;
-
+    
+    char ss[][5]={"RR","RNR","REJ","SREJ"};
+    char uc[][5]={"UI","SIM","-","SARM","UP","-","-","SABM","DISC","-","-","SARME","-","-","-","SABME","SNRM","-","-","RSET","-","-","-","XID"};
+    char ur[][5]={"UI","RIM","-","DM","-","-","-","-","RD","-","-","-","UA","-","-","-","-","FRMF","-","-","-","-","-","XID"};                                                                                                                                    // 11 SREJ
+    unsigned short int tot = (trama[12] << 8) | trama[13];
+	
+	printf("***********************Cabecera Ethernet***********************\n");
     printf("MAC destino: ");
-    for(char i = 0; i < 6; i++){
+    for(char i = 0; i < 5; i++){
         printf("%02x:", trama[i]);
     }
-
+	printf("%02x", trama[5]);
+	
     printf("\n\nMAC origen: ");
-    for(char i = 6; i < 12; i++){
+    for(char i = 6; i < 11; i++){
         printf("%02x:", trama[i]);
     }
+	printf("%02x", trama[11]);
 
     printf("\n\nTipo o Tamanio: ");
-    printf("%02x%02x", trama[12], trama[13]);
-    tot = (trama[12] << 8) + trama[13];
-    printf("\n\n El valor del ToT es: %c", tot);
+	
+    printf("\n\n El valor del ToT es: %d", tot);
 
     if(tot <= 1500){
         printf("\n\nTamano: \n\n");
-        printf("  SAPd: %02x     SAPo: %02x\n", trama[14], trama[15]);
-        printf("\n  Control: ");
+	printf(" Cabecera LLC");
 
         switch(trama[16] & 3){
 
             case 1:
-                printf("T-S");
+                printf("\n\n T-S");
+		        printf("\n\n El SS de la trama es: %s y su N(R) es: %d \n",ss[(trama[16] >> 2) & 3], trama[17] >> 1);
                 break;
             case 3:
-                printf("T-U");
+                printf("\n\n T-U \n");
+
                 break;
             default:
-                printf("T-I");
+                printf("\n\n T-I \n");
+                printf("\n\n EL N(S) de la trama es: %d y su N(R) es: %d \n", trama[16] >> 1, trama[17] >> 1);
                 break;
         }
 
-        printf("\n\nCRC: ");
-        for(char i = T_TRAMA -4; i < T_TRAMA; i++){
-            printf("%02x:", trama[i]);
-        }
-        printf("\n\n");
     }else{
-        printf("Tipo: ");
+        printf("\n\n  Tipo: ");
     }
 
 }
